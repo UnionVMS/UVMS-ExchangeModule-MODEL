7a3962dc5ccb5dcb6fd0e871e842282ae113ae30
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-09-22 14:16:13 +0200

    Fixed jndi paths

--- src/main/java/eu/europa/ec/fisheries/uvms/exchange/model/constant/ExchangeModelConstants.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/exchange/model/constant/ExchangeModelConstants.java
@@ -13,30 +13,30 @@ package eu.europa.ec.fisheries.uvms.exchange.model.constant;
 
 public class ExchangeModelConstants {
 
-    public static final String CONNECTION_FACTORY = "java:/ConnectionFactory";
+    public static final String CONNECTION_FACTORY = "ConnectionFactory";
     public static final String CONNECTION_TYPE = "javax.jms.MessageListener";
-    public static final String PLUGIN_EVENTBUS = "java:/jms/topic/EventBus";
+    public static final String PLUGIN_EVENTBUS = "jms/topic/EventBus";
     public static final String EVENTBUS_NAME = "EventBus";
 
     public static final String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
     public static final String DESTINATION_TYPE_TOPIC = "javax.jms.Topic";
 
-    public static final String EXCHANGE_MESSAGE_IN_QUEUE      = "java:/jms/queue/UVMSExchangeEvent";
+    public static final String EXCHANGE_MESSAGE_IN_QUEUE      = "jms/queue/UVMSExchangeEvent";
     public static final String EXCHANGE_MESSAGE_IN_QUEUE_NAME = "UVMSExchangeEvent";
-    public static final String EXCHANGE_RESPONSE_QUEUE        = "java:/jms/queue/UVMSExchange";
-    public static final String QUEUE_DATASOURCE_INTERNAL      = "java:/jms/queue/UVMSExchangeModel";
+    public static final String EXCHANGE_RESPONSE_QUEUE        = "jms/queue/UVMSExchange";
+    public static final String QUEUE_DATASOURCE_INTERNAL      = "jms/queue/UVMSExchangeModel";
 
     public static final String EXCHANGE_REGISTER_SERVICE = "EXCHANGE_REGISTRY";
     public static final String SERVICE_NAME = "ServiceName";
 
     public static final String MODULE_NAME = "exchange";
 
-    public static final String QUEUE_INTEGRATION_RULES = "java:/jms/queue/UVMSRulesEvent";
-    public static final String QUEUE_INTEGRATION_ASSET = "java:/jms/queue/UVMSAssetEvent";
-    public static final String QUEUE_INTEGRATION_AUDIT = "java:/jms/queue/UVMSAuditEvent";
+    public static final String QUEUE_INTEGRATION_RULES = "jms/queue/UVMSRulesEvent";
+    public static final String QUEUE_INTEGRATION_ASSET = "jms/queue/UVMSAssetEvent";
+    public static final String QUEUE_INTEGRATION_AUDIT = "jms/queue/UVMSAuditEvent";
 
     // For ack
-    public static final String MOVEMENT_RESPONSE_QUEUE = "java:/jms/queue/UVMSMovement";
-	public static final String ACTIVITY_RESPONSE_QUEUE = "java:/jms/queue/UVMSActivityEvent";
+    public static final String MOVEMENT_RESPONSE_QUEUE = "jms/queue/UVMSMovement";
+	public static final String ACTIVITY_RESPONSE_QUEUE = "jms/queue/UVMSActivityEvent";
 
 }
