403d2e072786df7d272bd96202ddc8d598813019
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-12-06 12:37:53 +0100

    Review by osdjup Build Dtos from model and change to releasebranch in poms

---
+++ src/main/java/eu/europa/ec/fisheries/schema/exchange/movement/v1/ReportMovementType.java
@@ -0,0 +1,134 @@
+
+package eu.europa.ec.fisheries.schema.exchange.movement.v1;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import eu.europa.ec.fisheries.schema.exchange.plugin.types.v1.PluginType;
+import org.w3._2001.xmlschema.Adapter1;
+
+
+/**
+ * <p>Java class for ReportMovementType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ReportMovementType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="pluginName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="pluginType" type="{urn:types.plugin.exchange.schema.fisheries.ec.europa.eu:v1}PluginType"/>
+ *         &lt;element name="timestamp" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ReportMovementType", propOrder = {
+    "pluginName",
+    "pluginType",
+    "timestamp"
+})
+@XmlSeeAlso({
+    SendMovementToPluginType.class,
+    SetReportMovementType.class
+})
+public abstract class ReportMovementType
+    implements Serializable
+{
+
+    private final static long serialVersionUID = 1L;
+    @XmlElement(required = true)
+    protected String pluginName;
+    @XmlElement(required = true)
+    protected PluginType pluginType;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(Adapter1 .class)
+    @XmlSchemaType(name = "dateTime")
+    protected Date timestamp;
+
+    /**
+     * Gets the value of the pluginName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    /**
+     * Sets the value of the pluginName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPluginName(String value) {
+        this.pluginName = value;
+    }
+
+    /**
+     * Gets the value of the pluginType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link PluginType }
+     *     
+     */
+    public PluginType getPluginType() {
+        return pluginType;
+    }
+
+    /**
+     * Sets the value of the pluginType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link PluginType }
+     *     
+     */
+    public void setPluginType(PluginType value) {
+        this.pluginType = value;
+    }
+
+    /**
+     * Gets the value of the timestamp property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Sets the value of the timestamp property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTimestamp(Date value) {
+        this.timestamp = value;
+    }
+
+}
