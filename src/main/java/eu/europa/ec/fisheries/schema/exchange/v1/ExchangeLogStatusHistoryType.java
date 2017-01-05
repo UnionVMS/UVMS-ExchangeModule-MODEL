efc9365632edad07f4cc7723f72d708871c03329
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2017-01-05 09:06:26 +0100

    Review by osdjup Build Dtos from model and change to releasebranch in poms

---
+++ src/main/java/eu/europa/ec/fisheries/schema/exchange/v1/ExchangeLogStatusHistoryType.java
@@ -0,0 +1,100 @@
+
+package eu.europa.ec.fisheries.schema.exchange.v1;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import org.w3._2001.xmlschema.Adapter1;
+
+
+/**
+ * <p>Java class for ExchangeLogStatusHistoryType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ExchangeLogStatusHistoryType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="timestamp" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *         &lt;element name="status" type="{urn:exchange.schema.fisheries.ec.europa.eu:v1}ExchangeLogStatusTypeType"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ExchangeLogStatusHistoryType", propOrder = {
+    "timestamp",
+    "status"
+})
+public class ExchangeLogStatusHistoryType
+    implements Serializable
+{
+
+    private final static long serialVersionUID = 1L;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(Adapter1 .class)
+    @XmlSchemaType(name = "dateTime")
+    protected Date timestamp;
+    @XmlElement(required = true)
+    protected ExchangeLogStatusTypeType status;
+
+    /**
+     * Gets the value of the timestamp property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Sets the value of the timestamp property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTimestamp(Date value) {
+        this.timestamp = value;
+    }
+
+    /**
+     * Gets the value of the status property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ExchangeLogStatusTypeType }
+     *     
+     */
+    public ExchangeLogStatusTypeType getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the value of the status property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ExchangeLogStatusTypeType }
+     *     
+     */
+    public void setStatus(ExchangeLogStatusTypeType value) {
+        this.status = value;
+    }
+
+}
