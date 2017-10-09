25561211fae1b792034846b592073dc92b507794
James Sörling <james.p.sorling@consid.se>
2017-10-09 09:23:32 +0200

    restore binary compatibility with version 4.0.0

--- src/main/java/eu/europa/ec/fisheries/uvms/exchange/model/mapper/ExchangeModuleRequestMapper.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/exchange/model/mapper/ExchangeModuleRequestMapper.java
@@ -243,6 +243,23 @@ public class ExchangeModuleRequestMapper {
         return JAXBMarshaller.marshallJaxBObjectToString(request);
     }
 
+    /**
+     * @deprecated  As of release 4.0.2, replaced by createFluxFAReportRequest(String message, String username, String fluxDFValue,Date date,
+     *                                              String messageGuid, PluginType pluginType, String senderReceiver, String onValue)
+     */
+    @Deprecated
+    public static String createFluxFAReportRequest(String message, String username, String fluxDFValue,Date date, String messageGuid,PluginType pluginType,String senderReceiver) throws ExchangeModelMarshallException {
+        SetFLUXFAReportMessageRequest request = new SetFLUXFAReportMessageRequest();
+        request.setMethod(ExchangeModuleMethod.SET_FLUX_FA_REPORT_MESSAGE);
+        request.setUsername(username);
+        request.setRequest(message);
+        request.setFluxDataFlow(fluxDFValue);
+        request.setDate(date);
+        request.setMessageGuid(messageGuid);
+        request.setPluginType(pluginType);
+        request.setSenderOrReceiver(senderReceiver);
+        return JAXBMarshaller.marshallJaxBObjectToString(request);
+    }
 
     public static String createFluxFAReportRequest(String message, String username, String fluxDFValue,Date date,
                                                    String messageGuid, PluginType pluginType, String senderReceiver, String onValue) throws ExchangeModelMarshallException {
